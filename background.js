064eca7ac5c4c5c80a6c111ac32fef73b1eeb6bc
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-08-27 23:35:13 +0530

    added exception handling

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.tabs.onActivated.addListener(() => {
         console.log(tabs[0].url);
         if (tabs[0].url.includes("www.youtube.com/watch"))
             chrome.tabs.update(tabs[0].id, {
-                url: "http://127.0.0.1:5500/ytshow/index.html" + "?video_url=" + tabs[0].url
+                url: "https://ytvideo-enul.vercel.app" + "?video_url=" + tabs[0].url
             });
     });
 });
